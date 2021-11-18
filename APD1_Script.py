77d5d76f871aa74276aa5cb9f70976add27c7ee7
Pineappleaki <55026493+Pineappleaki@users.noreply.github.com>
2021-11-18 15:50:30 +0000

    Added plotTimeSeries
    Added plotTimeSeries function along side a few more additions such as converting data to weekly.

--- APD1_Script.py
+++ APD1_Script.py
@@ -2,8 +2,14 @@
 Data obtained form: https://www.kaggle.com/amritpal333/crypto-mining-data
 Analyse the transactions comprised on the blockchain and compare ethereum to bitcoin
 """
+from datetime import date
+from colorama import Fore, Style
+from scipy import stats
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+
 
 def matchDates(data, date):
     """
@@ -62,6 +68,111 @@ def normalise(df):
             result[feature_name] = (df[feature_name] - min_value) / (max_value - min_value)
     return result
 
+
+def plotTimeSeries(data, y1, y2,
+                   labels=['Title', 'X-axis', 'Y-axis(left)', 'Y-axis(right)'],
+                   asset_set=['BTC', 'ETH'],
+                   color_set=['#2077b4', '#f38043'],
+                   save_file=False,
+                   produce_summary=False):
+    """
+    Parameters
+    ----------
+    data : List
+        An array of the desired datasets to plot.
+    y1 : String
+        Left Y axis value.
+    y2 : String
+        Right Y axis value.
+    labels : List
+        list of labels: Title;X;Y1;Y2, enter '' to skip a given element.
+    asset_set : List
+        set of each currency being plotted.
+    color_set : List
+        set of two colours.
+    save_file : Bool
+        Save file as a .png, default = False.
+    produce_summary : Bool
+        Produces summary stats (Min, Max, Mean) and the spearman between
+        y1 & y2, default = True.
+
+    Returns
+    -------
+    Shows a plot for each respective element in 'data', with option to save
+    graphics and produce a summary
+    """
+    print(f'\n*** {Fore.YELLOW + Style.BRIGHT}STARTING:{Fore.RESET} plotTimeSeries ***')
+    # Unpacking lists
+    try:
+        gen_title, x_label, y1_label, y2_label = labels
+    except:
+        print(f"{Fore.RED + Style.BRIGHT}ERROR:{Fore.RESET} Labels is missing {(4 - len(labels))} parameters")
+        print(f'*** {Fore.YELLOW + Style.BRIGHT}ENDING:{Fore.RESET} plotTimeSeries ***\n')
+        return
+    color_a, color_b = color_set
+
+    current_date = date.today().strftime('%d-%m-%Y')
+
+    # Establishing variables
+    itn = 0
+
+    for df in data:
+
+        title = f'{asset_set[itn]}: {gen_title}'
+        itn += 1
+
+        fig, ax1 = plt.subplots()
+
+        ax1.tick_params(axis='x', rotation=45)
+        ax1.title.set_text(title)
+        ax1.set_xlabel(x_label)
+        ax1.set_ylabel(y1_label)
+        ax1.grid(True)
+        ax1.fill_between(df.index, df[y1], alpha=0.25)
+
+        ax1.plot(df.index, df[y1], label=y1_label)
+
+        ax2 = ax1.twinx()
+        ax2.set_ylabel(y2_label)
+
+        ax2.plot(df.index, df[y2], color=color_set[1])
+
+        plt.show()
+
+        if save_file == True:
+            file_name = (f'./plots/{title}_{current_date}.png')
+            print(f'{Fore.MAGENTA + Style.BRIGHT}SAVING:{Fore.RESET} {file_name}')
+            fig.savefig((file_name),
+                        format='png',
+                        dpi=120,
+                        bbox_inches='tight')
+
+        if produce_summary == True:
+            print(f'\nProducing Summary for {Fore.YELLOW + Style.BRIGHT}{asset_set[itn-1]}:{Fore.RESET}\n')
+            all_features = [y1, y2]
+            total_summary = []
+            # Basic summary
+            for feature in all_features:
+                summary = []
+                summary.append(np.max(df[feature]))
+                summary.append(np.min(df[feature]))
+                summary.append(np.mean(df[feature]))
+
+                total_summary.append(summary)
+            feature_df = pd.DataFrame(total_summary, columns=(['Maximum', 'Minimum', 'Mean']),
+                                      index=(all_features))
+            feature_df = feature_df.transpose()
+
+            spearman = stats.spearmanr(df[y1], df[y2])
+            print(feature_df)
+            print('\nSpearman R Coefficient:\n',
+                  f'{Fore.BLUE + Style.BRIGHT}Correlation:{Fore.RESET} {spearman[0]}\n',
+                  f'{Fore.BLUE + Style.BRIGHT}P-Value:{Fore.RESET} {spearman[1]}\n')
+
+    print(f'*** {Fore.YELLOW + Style.BRIGHT}ENDING:{Fore.RESET} plotTimeSeries ***\n')
+    return
+
+
 # First we need to retrieve the files we will be working with
 btc_file = './data/bitcoin.csv'
 eth_file = './data/ethereum.csv'
@@ -78,6 +189,16 @@ btc, eth = data[0], data[1]
 btc_weekly = btc.resample('W').mean()
 eth_weekly = eth.resample('W').mean()
 
-# Normalising data
-btc_norm = normalise(btc_weekly)
-eth_norm = normalise(eth_weekly)
+# Normalising data - No longer needed.
+#btc_norm = normalise(btc_weekly)
+#eth_norm = normalise(eth_weekly)
+
+data1 = btc_weekly
+data2 = eth_weekly
+
+data = [data1, data2]
+plot_labels = ['Weekly Price & TX Count', 'Date', 'Price (USD)',
+          'TX Count']
+asset = ['BTC', 'ETH']
+
+plotTimeSeries(data, 'price(USD)', 'txCount', plot_labels)
